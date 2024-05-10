be26e1342b765d4bb09f567598e6d2c02075e280
yuoiiii44 <sheaoliverpun0suy@hotmail.com>
2024-05-10 23:42:14 +0700

    add more features

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const drop = (arr, n = 1) => arr.slice(n);
+function firstMissingPositive(nums) {
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      const temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) return i + 1;
+  }
+  return n + 1;
+}
